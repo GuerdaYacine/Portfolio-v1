300fbfdf51c35db0cd573deea773d05c7e4fdd1a
Yacine <guerda.yacine60100@gmail.com>
2025-04-27 03:13:59 +0200

    fix about for mobile

--- app/pages/about.php
+++ app/pages/about.php
@@ -179,7 +179,7 @@
             </div>
         </aside>
     </div>
-    <div class="absolute bottom-[50px] left-[50%] right-[50%]  table">
+    <div class="absolute md:bottom-[50px] bottom-[100px] left-[50%] right-[50%]  table">
         <div class="relative h-[34px] w-[22px] border-2 border-[#eff0f3] rounded-full">
             <div class="absolute top-2 left-0 right-0 mx-auto h-[4px] w-[4px] bg-[#eff0f3] rounded-full animate-scroll-down">
             </div>
